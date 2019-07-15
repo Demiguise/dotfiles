4a7363be556bb1ee619a576448202ec5b6ed1883
Andy <andy.c.hewitson@gmail.com>
2019-07-15 18:54:51 +0200

    Cleaning up theme file

--- awesome/themes/karak/theme.lua
+++ awesome/themes/karak/theme.lua
@@ -8,15 +8,17 @@ local theme_name = "karak"
 local theme_folder = themes_path .. theme_name .. "/"
 local dpi = require("beautiful.xresources").apply_dpi
 
--- {{{ Main
 local theme = {}
 theme.wallpaper = theme_folder .. "background.png"
--- }}}
 
--- {{{ Styles
-theme.font      = "sans 8"
+------------------------------------------------------------------------
+-- Styling
 
--- {{{ Colors
+theme.font = "sans 8"
+
+------------------------------------------------------------------------
+-- Colours
+
 theme.fg_normal  = "#DCDCCC"
 theme.fg_focus   = "#F0DFAF"
 theme.fg_urgent  = "#CC9393"
@@ -24,68 +26,45 @@ theme.bg_normal  = "#3F3F3F"
 theme.bg_focus   = "#1E2320"
 theme.bg_urgent  = "#3F3F3F"
 theme.bg_systray = theme.bg_normal
--- }}}
 
--- {{{ Borders
+------------------------------------------------------------------------
+-- Borders
+
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
 theme.border_normal = "#3F3F3F"
 theme.border_focus  = "#6F6F6F"
 theme.border_marked = "#CC9393"
--- }}}
 
--- {{{ Titlebars
+------------------------------------------------------------------------
+-- Titlebars
+
 theme.titlebar_bg_focus  = "#3F3F3F"
 theme.titlebar_bg_normal = "#3F3F3F"
--- }}}
 
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- [taglist|tasklist]_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- titlebar_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- Example:
---theme.taglist_bg_focus = "#CC9393"
--- }}}
+------------------------------------------------------------------------
+--  Mouse finder
 
--- {{{ Widgets
--- You can add as many variables as
--- you wish and access them by using
--- beautiful.variable in your rc.lua
---theme.fg_widget        = "#AECF96"
---theme.fg_center_widget = "#88A175"
---theme.fg_end_widget    = "#FF5656"
---theme.bg_widget        = "#494B4F"
---theme.border_widget    = "#3F3F3F"
--- }}}
-
--- {{{ Mouse finder
 theme.mouse_finder_color = "#CC9393"
--- mouse_finder_[timeout|animate_timeout|radius|factor]
--- }}}
 
--- {{{ Menu
--- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
+------------------------------------------------------------------------
+--  Menu
+
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
--- }}}
 
--- {{{ Icons
--- {{{ Taglist
+------------------------------------------------------------------------
+--  Icons
+
+--  Taglist
 theme.taglist_squares_sel   = theme_folder .. "taglist/squarefz.png"
 theme.taglist_squares_unsel = theme_folder .. "taglist/squarez.png"
---theme.taglist_squares_resize = "false"
--- }}}
 
--- {{{ Misc
+-- Misc
 theme.awesome_icon           = theme_folder .. "awesome-icon.png"
 theme.menu_submenu_icon      = theme_folder .. "default/submenu.png"
--- }}}
 
--- {{{ Layout
+-- Layout
 theme.layout_tile       = theme_folder .. "layouts/tile.png"
 theme.layout_tileleft   = theme_folder .. "layouts/tileleft.png"
 theme.layout_tilebottom = theme_folder .. "layouts/tilebottom.png"
@@ -102,9 +81,8 @@ theme.layout_cornernw   = theme_folder .. "layouts/cornernw.png"
 theme.layout_cornerne   = theme_folder .. "layouts/cornerne.png"
 theme.layout_cornersw   = theme_folder .. "layouts/cornersw.png"
 theme.layout_cornerse   = theme_folder .. "layouts/cornerse.png"
--- }}}
 
--- {{{ Titlebar
+-- Titlebar
 theme.titlebar_close_button_focus  = theme_folder .. "titlebar/close_focus.png"
 theme.titlebar_close_button_normal = theme_folder .. "titlebar/close_normal.png"
 
@@ -130,9 +108,5 @@ theme.titlebar_maximized_button_focus_active  = theme_folder .. "titlebar/maximi
 theme.titlebar_maximized_button_normal_active = theme_folder .. "titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = theme_folder .. "titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = theme_folder .. "titlebar/maximized_normal_inactive.png"
--- }}}
--- }}}
 
 return theme
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
