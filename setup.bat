ca9748e14c42dc96d242148a82a10dfc46ea2177
Andrew Hewitson <andrew.hewitson@omegapoint.se>
2021-03-18 11:37:29 +0100

    Fix directory issues with batch scripts

--- setup.bat
+++ setup.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
 
 set USERDIR=%HOMEDRIVE%%HOMEPATH%
-set WORKINGDIR=%CD%
+set WORKINGDIR=%~dp0
 
+echo =====
 echo Setting up dotfiles for [%USERDIR%] from [%WORKINGDIR%]
+echo =====
 
 echo Linking [%WORKINGDIR%\git\.gitconfig] to [%USERDIR%\.gitconfig]
 mklink %USERDIR%\.gitconfig %WORKINGDIR%\git\.gitconfig
