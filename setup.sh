ab2251ff0ec9e456b7b55c6145dca3f1a5670f8f
Andrew Hewitson <andrew.hewitson@omegapoint.se>
2021-03-26 10:51:00 +0100

    Added linux setup script

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+USER_HOME=$(getent passwd $USER | cut -d: -f6)
+
+if [[ "$EUID" -eq 0 ]]; then
+	USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+fi
+
+echo Working Dir: ${WORKING_DIR}
+echo User Dir: ${USER_HOME}
+
+# Expects a two file paths to link
+onCreateSymlink()
+{
+	echo -e "\t * Symlinking ${WORKING_DIR}/$1 => $2"
+	linkDir=$(dirname $2)
+	if [ ! -d "$linkDir" ]; then
+		echo -e "\t * Making directory..."
+		mkdir -p $linkDir
+	fi
+	if [ -d "$2" ]; then
+		echo -e "\t * Ignoring existing symlink"
+	else
+		sudo ln -sf ${WORKING_DIR}/$1 $2
+	fi
+}
+
+onCreateFile()
+{
+	echo -e "\t * Touching $1"
+	touch $1
+}
+
+onCreateSymlink git/.gitconfig ${USER_HOME}/.gitconfig
+onCreateSymlink vim/.vimrc ${USER_HOME}/.vimrc
+onCreateSymlink vim/.vim ${USER_HOME}/.vim
+
+onCreateFile ${WORKING_DIR}/vim/.vim/local.vim
+onCreateFile ${USER_HOME}/.device.gitconfig
